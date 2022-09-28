0bcd86c5b923e3f27767afdb39d9ff325c62b4f2
bes <pas1007@naver.com>
2022-09-28 21:09:35 +0900

    PostService 클래스 생성 (save, update)

---
+++ src/main/java/springboot2/webservice/service/posts/PostsService.java
@@ -0,0 +1,41 @@
+package springboot2.webservice.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import springboot2.webservice.domain.posts.Posts;
+import springboot2.webservice.domain.posts.PostsRepository;
+import springboot2.webservice.web.dto.PostsResponseDto;
+import springboot2.webservice.web.dto.PostsSaveRequestDto;
+import springboot2.webservice.web.dto.PostsUpdateRequestDto;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto
+                       requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+}
